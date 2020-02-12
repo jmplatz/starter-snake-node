9db3ba3a41d4cd796e8f437a47a37d72b207e9b7
Jordan <jordan.platz1@gmail.com>
2020-02-11 21:33:29 -0800

    trying with regular for loop

--- index.js
+++ index.js
@@ -44,12 +44,13 @@ app.post('/move', (request, response) => {
   let board = Array(request.body.board.height).fill().map(
     () => Array(request.body.board.width).fill(0));
 
-  board.forEach(row => {
-    row.forEach(col => {
-      if (row === 0)
-        board[row.y][col.x] = 1;
-    });
-  });
+  for (let i = 0; i < board.length; i++) {
+    for (let k = 0; k < board[i].length; k++) {
+      if (i == 0) {
+        board[i][k] = 1;
+      }
+    }
+  }
 
 
   // coords for my snake's head
