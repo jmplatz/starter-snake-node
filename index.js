e99f912c46a336c7b0238b71b5a48cab79d8a438
Jordan <jordan.platz1@gmail.com>
2020-02-18 18:52:58 -0800

    Auto stash before merge of "Functional" and "origin/Functional"

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const logger = require('morgan');
-const easystarjs = require('easystarjs');
+/** @format */
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const logger = require("morgan");
+const easystarjs = require("easystarjs");
 const app = express();
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
   poweredByHandler
-} = require('./handlers.js');
+} = require("./handlers.js");
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001));
+app.set("port", process.env.PORT || 9001);
 
-app.enable('verbose errors');
+app.enable("verbose errors");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(poweredByHandler);
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
 // Handle POST request to '/start'
-app.post('/start', (request, response) => {
+app.post("/start", (request, response) => {
   // NOTE: Do something here to start the game
 
   // Response data
   const data = {
-    "color": "#ff00ff",
-    "headType": "bendr",
-    "tailType": "pixel",
+    color: "#ff00ff",
+    headType: "bendr",
+    tailType: "pixel"
   };
 
   return response.json(data);
 });
 
 // Handle POST request to '/move'
-app.post('/move', (request, response) => {
+app.post("/move", (request, response) => {
   const easystar = new easystarjs.js();
 
   const data = {
-    move: 'up' // default to up
+    move: "up" // default to up
   };
 
   const mySnake = {
@@ -55,8 +57,9 @@ app.post('/move', (request, response) => {
     const boardHeight = request.body.board.height;
     const boardWidth = request.body.board.width;
 
-    const board = Array(boardHeight).fill().map(
-      () => Array(boardWidth).fill(0));
+    const board = Array(boardHeight)
+      .fill()
+      .map(() => Array(boardWidth).fill(0));
 
     const mySnake = request.body.you.body;
     const mySnakeBody = mySnake.splice(1);
@@ -109,8 +112,15 @@ app.post('/move', (request, response) => {
     const foodLocations = request.body.board.food;
 
     for (let i = 0; i < foodLocations.length; i++) {
+<<<<<<< Updated upstream
       let moveDistance = Math.abs(mySnakeHead.x - foodLocations[i].x) + Math.abs(mySnakeHead.y - foodLocations[i].y);
       foodMoveDistances.push(moveDistance);
+=======
+      let moveDistance =
+        Math.abs(mySnakeHead.x - foodLocations[i].x) +
+        Math.abs(mySnakeHead.y - foodLocations[i].y);
+      foodArray.push(moveDistance);
+>>>>>>> Stashed changes
     }
 
     // Index of each food object
@@ -135,46 +145,58 @@ app.post('/move', (request, response) => {
   easystar.enableSync(); // required to work
 
   let nextMove = [];
-  easystar.findPath(mySnakeHead.x, mySnakeHead.y, closestFood.x, closestFood.y, function (path) {
-    if (path === null) {
-      console.log("Could not find path to closest food. Activate panic mode.");
-    } else {
-      nextMove = path;
+  easystar.findPath(
+    mySnakeHead.x,
+    mySnakeHead.y,
+    closestFood.x,
+    closestFood.y,
+    function(path) {
+      if (path === null) {
+        console.log(
+          "Could not find path to closest food. Activate panic mode."
+        );
+      } else {
+        nextMove = path;
+      }
     }
-  });
+  );
 
   easystar.calculate();
 
   // Returns move
   if (mySnakeHead.x > nextMove[1].x) {
-    data.move = 'left';
+    data.move = "left";
   } else if (mySnakeHead.x < nextMove[1].x) {
-    data.move = 'right';
+    data.move = "right";
   } else if (mySnakeHead.y < nextMove[1].y) {
-    data.move = 'down';
+    data.move = "down";
   } else if (mySnakeHead.y > nextMove[1].y) {
-    data.move = 'up';
+    data.move = "up";
   }
 
   return response.json(data);
 });
 
+<<<<<<< Updated upstream
 app.post('/end', (request, response) => {
+=======
+app.post("/end", (request, response) => {
+>>>>>>> Stashed changes
   // NOTE: Any cleanup when a game is complete.
   return response.json({});
 });
 
-app.post('/ping', (request, response) => {
+app.post("/ping", (request, response) => {
   // Used for checking if this snake is still alive.
   return response.json({});
 });
 
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
-app.use('*', fallbackHandler);
+app.use("*", fallbackHandler);
 app.use(notFoundHandler);
 app.use(genericErrorHandler);
 
-app.listen(app.get('port'), () => {
-  console.log('Server listening on port %s', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Server listening on port %s", app.get("port"));
 });
